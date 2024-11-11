400cd4e0edb1894f69721ece652a8d78258a7c84
almostDemoPy <dazanderofficials@gmail.com>
2024-11-11 13:43:39 +0800

    ApplicationEventWebhookStatus[Enum]

--- democord/enums.py
+++ democord/enums.py
@@ -2,6 +2,12 @@ from enum   import Enum
 from typing import *
 
 
+class ApplicationEventWebhookStatus(Enum):
+  disabled            = 1
+  enabled             = 2
+  disabled_by_discord = 3
+
+
 class ApplicationIntegrationType(Enum):
   guild_install = 0
   user_intall   = 1
