2e25fe16e843d9e6f58e1f1b60436d638439208b
almostDemoPy <dazanderofficials@gmail.com>
2024-11-11 13:44:17 +0800

    ApplicationFlag[IntFlag]

---
+++ democord/flags.py
@@ -0,0 +1,15 @@
+from enum   import IntFlag
+from typing import *
+
+
+class ApplicationFlag(IntFlag):
+  application_auto_moderation_rule_create_badge = 1 <<  6
+  gateway_presence                              = 1 << 12
+  gateway_presence_limited                      = 1 << 13
+  gateway_guild_members                         = 1 << 14
+  gateway_guild_members_limited                 = 1 << 15
+  verification_pending_guild_limit              = 1 << 16
+  embedded                                      = 1 << 17
+  gateway_message_content                       = 1 << 18
+  gateway_message_content_limited               = 1 << 19
+  application_command_badge                     = 1 << 23
