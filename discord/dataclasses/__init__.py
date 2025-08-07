b14184d2b13420dbb71c6fe0e822890832a17ec7
almostDemoPy <dazanderofficials@gmail.com>
2025-08-07 21:49:28 +0800

    Added 'EmbedImage' class

--- discord/dataclasses/__init__.py
+++ discord/dataclasses/__init__.py
@@ -14,6 +14,7 @@ from .ban import Ban
 from .channel import Channel
 from .default_reaction import DefaultReaction
 from .embed import Embed
+from .embed_image import EmbedImage
 from .embed_thumbnail import EmbedThumbnail
 from .embed_video import EmbedVideo
 from .emoji import Emoji
